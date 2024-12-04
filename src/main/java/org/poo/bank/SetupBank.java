bd68e3d358148c8ba05aa22ae5fb91250f1e9e2c
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-12-04 23:50:20 +0200

    Starting point of my project Implement input read and hashmaps for users and accounts

---
+++ src/main/java/org/poo/bank/SetupBank.java
@@ -0,0 +1,48 @@
+package org.poo.bank;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import lombok.Getter;
+import lombok.Setter;
+import org.poo.actions.Command;
+import org.poo.fileio.CommandInput;
+import org.poo.fileio.ExchangeInput;
+import org.poo.fileio.ObjectInput;
+import org.poo.fileio.UserInput;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+public class SetupBank {
+    private Map<String, User> users = new HashMap<>();
+    private Map<String, Account> accounts = new HashMap<>();
+    private ExchangeRates exchangeRates = new ExchangeRates();
+    private ArrayNode output;
+
+    public SetupBank(ObjectInput input, ArrayNode output) {
+        for (UserInput userInput : input.getUsers()) {
+            User user = new User(userInput);
+            users.put(user.getEmail(), user);
+        }
+
+        for (ExchangeInput exchangeInput : input.getExchangeRates()) {
+            exchangeRates.addRate(exchangeInput.getFrom(), exchangeInput.getTo(),
+                                    exchangeInput.getRate());
+        }
+        this.output = output;
+    }
+
+    public void performCommands(CommandInput input) {
+        switch (input.getCommand()) {
+            case "printUsers":
+                Command.printUsers(this, input, output);
+                break;
+            case "addAccount":
+                Command.addAccount(this, input);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid command");
+        }
+    }
+}
