c9d17ad4349b97c9073328b412712e806d4d56d8
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-12-07 12:41:20 +0200

    Implement logic for convert between currencies

--- src/main/java/org/poo/bank/ExchangeRates.java
+++ src/main/java/org/poo/bank/ExchangeRates.java
@@ -11,9 +11,41 @@ public class ExchangeRates {
             graph.put(fromCurrency, new HashMap<>());
         }
         graph.get(fromCurrency).put(toCurrency, rate);
+
+        if (!graph.containsKey(toCurrency)) {
+            graph.put(toCurrency, new HashMap<>());
+        }
+        graph.get(toCurrency).put(fromCurrency, 1 / rate);
+    }
+
+    private void calculateAllRates() {
+        for (String k : graph.keySet()) {
+            for (String i : graph.keySet()) {
+                for (String j : graph.keySet()) {
+                    if (graph.get(i).containsKey(k) && graph.get(k).containsKey(j)) {
+                        double indirectRate = graph.get(i).get(k) * graph.get(k).get(j);
+
+                        if (!graph.get(i).containsKey(j)) {
+                            graph.get(i).put(j, indirectRate);
+                            graph.get(j).put(i, 1 / indirectRate);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public double getRate(String fromCurrency, String toCurrency) {
+        if (fromCurrency.equals(toCurrency)) {
+            return 1;
+        }
+
+        if (graph.containsKey(fromCurrency) && graph.get(fromCurrency).containsKey(toCurrency)) {
+            return graph.get(fromCurrency).get(toCurrency);
+        }
+
+        // If the rate is not directly available, calculate it
+        calculateAllRates();
         return graph.get(fromCurrency).get(toCurrency);
     }
 }
