ae16434609e9a65e069334c59e8d10071e7cc427
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-12-07 12:39:02 +0200

    Add method for account return and for withdraw money

--- src/main/java/org/poo/bank/Account.java
+++ src/main/java/org/poo/bank/Account.java
@@ -13,9 +13,11 @@ public abstract class Account {
     public static final double DEFAULT_BALANCE = 0.0;
     private String iban;
     private double balance;
+    private double minBalance;
     private String currency;
     private String accountType;
     private ArrayList<Card> cards = new ArrayList<>();
+    private User owner;
 
     public Account(CommandInput commandInput) {
         iban = Utils.generateIBAN();
@@ -34,6 +36,20 @@ public abstract class Account {
 
     public void deleteCard(Card card) {
         cards.remove(card);
+    }
+
+    public static Account getAccount(SetupBank bank, String iban) {
+        return bank.getAccounts().get(iban);
+    }
+
+    public double withdraw(double amount) {
+        if (balance - amount < minBalance) {
+            // update() transactions with an error message
+            return 0;
+            //throw new IllegalArgumentException("Not enough funds");
+        }
+        balance -= amount;
+        return amount;
     }
 }
 
