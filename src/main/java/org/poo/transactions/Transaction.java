6869630751129b5667affd99efc108a054635f4d
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-12-09 00:07:26 +0200

    Add class for transaction I use builder design pattern

---
+++ src/main/java/org/poo/transactions/Transaction.java
@@ -0,0 +1,121 @@
+package org.poo.transactions;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Transaction {
+    private final long timestamp;
+    private final String description;
+    private final String card;
+    private final String cardHolder;
+    private final String account;
+    private final String senderIBAN;
+    private final String receiverIBAN;
+    private final String transferType;
+    private final String commerciant;
+
+    @JsonIgnore
+    private final Double amount;
+    @JsonIgnore
+    private final String amountString;
+
+    private Transaction(TransactionBuilder builder) {
+        this.timestamp = builder.timestamp;
+        this.description = builder.description;
+        this.card = builder.card;
+        this.cardHolder = builder.cardHolder;
+        this.account = builder.account;
+        this.senderIBAN = builder.senderIBAN;
+        this.receiverIBAN = builder.receiverIBAN;
+        this.amount = builder.amount;
+        this.amountString = builder.amountString;
+        this.transferType = builder.transferType;
+        this.commerciant = builder.commerciant;
+    }
+
+    @JsonProperty("amount")
+    public Object getDynamicAmount() {
+        if (amountString != null) {
+            return amountString;
+        }
+        if (amount != null) {
+            return amount;
+        }
+        return null;
+    }
+
+    public static class TransactionBuilder {
+        private final long timestamp;
+        private final String description;
+        private String card;
+        private String cardHolder;
+        private String account;
+        private String senderIBAN;
+        private String receiverIBAN;
+        private Double amount;
+        private String amountString;
+        private String transferType;
+        private String commerciant;
+
+        public TransactionBuilder(long timestamp, String description) {
+            this.timestamp = timestamp;
+            this.description = description;
+        }
+
+        public TransactionBuilder card(String card) {
+            this.card = card;
+            return this;
+        }
+
+        public TransactionBuilder cardHolder(String cardHolder) {
+            this.cardHolder = cardHolder;
+            return this;
+        }
+
+        public TransactionBuilder account(String account) {
+            this.account = account;
+            return this;
+        }
+
+        public TransactionBuilder senderIBAN(String senderIBAN) {
+            this.senderIBAN = senderIBAN;
+            return this;
+        }
+
+        public TransactionBuilder receiverIBAN(String receiverIBAN) {
+            this.receiverIBAN = receiverIBAN;
+            return this;
+        }
+
+        public TransactionBuilder amount(Double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public TransactionBuilder amountString(String amountString) {
+            this.amountString = amountString;
+            return this;
+        }
+
+        public TransactionBuilder transferType(String transferType) {
+            this.transferType = transferType;
+            return this;
+        }
+
+        public TransactionBuilder commerciant(String commerciant) {
+            this.commerciant = commerciant;
+            return this;
+        }
+
+        public Transaction build() {
+            return new Transaction(this);
+        }
+    }
+}
